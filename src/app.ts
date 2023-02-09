be07f782fbdd8aacbe823eb5897f81ab000c04eb
New Panuwat <new_sbh@me.com>
2023-02-09 21:06:28 +0700

    add typeorm

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,13 @@
 import express = require("express");
 import cors = require("cors");
-
-console.log("hello bt");
+import "reflect-metadata";
 
 const app = express();
 
 app.use(cors({ origin: ["http://localhost:3000", "http://localhost:8080", "http://localhost:4200"] }));
+
+app.use(express.json());
+
+console.log("listening to port 8000");
 
 app.listen(8000);
